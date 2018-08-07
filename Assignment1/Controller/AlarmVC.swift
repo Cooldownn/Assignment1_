8225b51f9a58253375a0e51abb2b05452ea86460
Cooldown <dphung291@gmail.com>
2018-08-07 16:56:00 +1000

    Initial Commit

---
+++ Assignment1/Controller/AlarmVC.swift
@@ -0,0 +1,22 @@
+//
+//  AlarmVC.swift
+//  Assignment1
+//
+//  Created by Cooldown on 7/8/18.
+//  Copyright © 2018 Cooldown. All rights reserved.
+//
+
+import UIKit
+
+class AlarmVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backtoAlarm(unwind: UIStoryboardSegue) {
+        
+    }
+}
